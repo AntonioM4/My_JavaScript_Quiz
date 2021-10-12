3d67d4570c745af4468cc925e2882acc79d37cd2
Antonio Marino <marino.antonio375@gmail.com>
2021-10-12 18:21:44 -0400

    added first question and finished styling

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,30 +1,32 @@
-var questions = [{
-  question: "Who invented JavaScript?",
-  answers: {
-    a: "Douglas Crockford",
-    b: "Sheryl Sandberg",
-    c: "Brendan Eich"
-  },
-  correctAnswer: "c"
-},
-{
-  question: "Which one of these is a JavaScript package manager?",
-  answers: {
-    a: "Node.js",
-    b: "TypeScript",
-    c: "npm"
-  },
-  correctAnswer: "c"
-},
-{
-  question: "Which tool can you use to ensure code quality?",
-  answers: {
-    a: "Angular",
-    b: "jQuery",
-    c: "RequireJS",
-    d: "ESLint"
-  },
-  correctAnswer: "d"
-  }
-]; 
+var startButton = document.getElementById('start-btn');
+var questionContainerElement = document.getElementById('questions-container')
+var shuffledQuestions, currentQuestionsIndex
 
+
+startButton.addEventListener('click', startQuiz)
+
+function startQuiz(){
+  console.log('started')
+  startButton.classList.add('hide')
+  questionContainerElement.classList.remove('hide')
+}
+
+function setNextQuestion(){
+
+}
+
+function selectAnswer(){
+
+}
+
+var questions = [
+  {
+    question: 'Which of the following is an advantage of using JavaScript?',
+    answers: [
+      {text: 'Less server interaction', correct: false },
+      {text: 'Immediate feedback to the visitors', correct: false },
+      {text: 'Increased interactivity', correct: false },
+      {text: 'All of the above.', correct: true},
+    ] 
+  }
+]
