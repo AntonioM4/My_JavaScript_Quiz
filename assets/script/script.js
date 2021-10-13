e5edf9dc7901137b71f5a581acd917b038e012cd
Antonio Marino <marino.antonio375@gmail.com>
2021-10-12 22:12:26 -0400

    hit large roadblock need to rework from here

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,24 +1,28 @@
 var startButton = document.getElementById('start-btn');
 var nextButton = document.getElementById('next-btn');
-var questionContainerElement = document.getElementById('questions-container')
-var questionElement = document.getElementById('questions')
-var answerButtonsElement = document.getElementById('answer-buttons')
-
+var questionContainerElement = document.getElementById('questions-container');
+var questionElement = document.getElementById('questions');
+var answerButtonsElement = document.getElementById('answer-buttons');
+var resultsButton = document.getElementById('results-btn')
+var score = 0; 
+var userAnswers = [];
 
 let shuffledQuestions, currentQuestionIndex
 
 
-startButton.addEventListener('click', startQuiz)
-nextButton.addEventListener('click', ()=>{
+startButton.addEventListener('click', startQuiz);
+nextButton.addEventListener('click', ()=> {
   currentQuestionIndex++;
   setNextQuestion();
 })
+// resultsButton.addEventListener('click', results);
 
 function startQuiz() { 
   startButton.classList.add('hide');
-  questionContainerElement.classList.remove('hide');
-  shuffledQuestions = question.sort(()=> Math.random()- .5);
+  console.log(shuffledQuestions)
+  shuffledQuestions = question.sort(()=> Math.random() - .5);
   currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
   setNextQuestion(); 
 }
 
@@ -36,6 +40,10 @@ function showQuestion(question) {
     button.classList.add('btn')
     if (answers.correct){
       button.dataset.correct = answers.correct
+      score ++;
+      console.log(score)
+    }else{
+      (score - 1)
     }
     button.addEventListener('click', selectAnswer);
     answerButtonsElement.appendChild(button);
@@ -61,9 +69,14 @@ function selectAnswer(e) {
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
     nextButton.classList.remove('hide')
   }else{
-    nextButton.innerText = 'Results';
-    nextButton.classlist.remove('hide');
+    resultsButton.classList.remove('hide');
   }
+}
+
+
+
+function results() {
+ console.log('Results here')
 }
 
 function setStatusClass(element, correct) {
@@ -75,11 +88,14 @@ function setStatusClass(element, correct) {
       }
 } 
 
+
+
 function clearStatusClass(element) {
   element.classList.remove('correct')
   element.classList.remove('wrong')
   
 }
+
 
 
 var question = [
@@ -97,17 +113,35 @@ var question = [
     answers: [
     {text: 'window.print();', correct: true},
     {text: 'browser.print();', correct: false },
-    {text: 'browser.print();', correct: false },
-    {text: 'browser.print();', correct: false },
+    {text: 'navigator.print();', correct: false },
+    {text: 'document.print();', correct: false },
     ]
   }
   ,{
-    question: 'Which of the following is the correct syntax to print a page using JavaScript?', 
+    question: 'Which of the following is correct about callbacks?', 
     answers: [
-    {text: 'window.print();', correct: true},
-    {text: 'browser.print();', correct: false },
-    {text: 'browser.print();', correct: false },
-    {text: 'browser.print();', correct: false },
+    {text: 'A callback is a plain JavaScript function passed to some method as an argument or option.', correct: false},
+    {text: 'Some callbacks are just events, called to give the user a chance to react when a certain state is triggered.', correct: false },
+    {text: 'Both of the above.', correct: true },
+    {text: 'None of the above.', correct: false },
+    ]
+  }
+  ,{
+    question: 'Which built-in method returns the string representation of the numbers value?',
+    answers: [
+    {text: 'toValue()', correct: false},
+    {text: 'toNumber()', correct: false },
+    {text: 'toString()', correct: true },
+    {text: 'None of the above.', correct: false },
+    ]
+  }
+  ,{
+    question: 'Which of the following function of Boolean object returns the primitive value of the Boolean object?',
+    answers: [
+    {text: 'toSource()', correct: false},
+    {text: 'valueOf()', correct: true },
+    {text: 'toString()', correct: false },
+    {text: 'None of the above.', correct: false },
     ]
   }
 ]
